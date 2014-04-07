2b05fb77a798aa2b958a0bd306a1a2ee81f9e4cc
melodicodyssey <stevedbauer@gmail.com>
2014-04-07 13:07:29 -0700

    coded angular router/controller + navbar/jumbotron

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 console.log("Hello World");
+
+var myApp = angular.module("MyApp", []);
+
+var myRouter = angular.module("MyRouter", ["ngRoute"]);
+
+myRouter.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/', {
+      templateUrl: 'assets/templates/home.html',
+      controller: 'MyCtrl'
+    }).
+    when('/about', {
+      templateUrl: 'assets/templates/about.html',
+      controller: 'MyCtrl'
+    }).
+    wen('/projects', {
+      templateUrl: 'assets/templates/projects.html',
+      controller: 'MyCtrl'
+    }).
+    otherwise({
+      redirectTo: '/'
+    })
+}]);
+
+myApp.controller("MyCtrl", ["$scope", "$location", function($scope, $location) {
+  $scope.go = function(path) {
+    $location.path(path);
+  }
+}])
+
