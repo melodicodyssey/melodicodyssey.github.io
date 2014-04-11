20b6056a7a3e906dc45ff5aa76102b2496f0db0d
melodicodyssey <stevedbauer@gmail.com>
2014-04-10 17:52:42 -0700

    working page

--- main.js
+++ main.js
@@ -1,32 +1,47 @@
-console.log("Hello World");
+// console.log("Hello World");
 
-var myApp = angular.module("MyApp", []);
+// var myApp = angular.module("MyApp", []);
 
-var myRouter = angular.module("MyRouter", ["ngRoute"]);
+// var myRouter = angular.module("MyRouter", ["ngRoute"]);
 
-myRouter.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-    when('/#/', {
-      templateUrl: 'assets/templates/home.html',
-      controller: 'MyCtrl'
-    }).
-    when('/#/about', {
-      templateUrl: 'assets/templates/about.html',
-      controller: 'MyCtrl'
-    }).
-    wen('/#/projects', {
-      templateUrl: 'assets/templates/projects.html',
-      controller: 'MyCtrl'
-    }).
-    otherwise({
-      redirectTo: '/'
-    })
-}]);
+// myRouter.config(['$routeProvider', function($routeProvider) {
+//   $routeProvider.
+//     when('/#/', {
+//       templateUrl: 'assets/templates/home.html',
+//       controller: 'MyCtrl'
+//     }).
+//     when('/#/about', {
+//       templateUrl: 'assets/templates/about.html',
+//       controller: 'MyCtrl'
+//     }).
+//     wen('/#/projects', {
+//       templateUrl: 'assets/templates/projects.html',
+//       controller: 'MyCtrl'
+//     }).
+//     otherwise({
+//       redirectTo: '/'
+//     });
+// }]);
 
-myApp.controller("MyCtrl", ["$scope", "$location", function($scope, $location) {
-  $scope.go = function(path) {
-    $location.path(path);
-  }
-  $scope.test = "Hello World";
-}])
+// myApp.controller("MyCtrl", ["$scope", "$location", function($scope, $location) {
+//   $scope.go = function(path) {
+//     $location.path(path);
+//   };
+//   $scope.test = "Hello World";
+// }]);
 
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
