40c3add27f66a427d8ae3bcd1fa51ba5d4b8ad3c
Stephen Bauer <stevedbauer@gmail.com>
2015-06-30 16:27:08 -0700

    replacing HTML, CSS and JS with re-design from Ro -- github.com/azureowl

--- main.js
+++ main.js
@@ -1,46 +1,46 @@
-// console.log("Hello World");
+Skip to content
+This repository  
+Pull requests
+Issues
+Gist
+ @melodicodyssey
+ Unwatch 1
+  Star 0
+  Fork 1
+melodicodyssey/Steve-Portfolio---Redesigned
+forked from azureowl/Steve-Portfolio---Redesigned
+ branch: master  Steve-Portfolio---Redesigned/StevePortfolio.js
+@azureowlazureowl an hour ago Edit copy, separated CSS and js in new files
+1 contributor
+RawBlameHistory     29 lines (27 sloc)  0.932 kB
+$(document).ready(function(){
+    $(".share-nav").on("click", function () {
+      showInfo($(this), ".info-share");
+    });
+    $(".aquaintly-nav").on("click", function () {
+      showInfo($(this), ".info-aquaintly");
+    });
+    $(".gnosis-nav").on("click", function () {
+      showInfo($(this), ".info-gnosis");
+    });
+    $(".bio-nav").on("click", function () {
+      showInfo($(this), ".info-bio");
+    });
+    $(".contact-nav").on("click", function () {
+      showInfo($(this), ".info-contact");
+    });
 
-// var myApp = angular.module("MyApp", []);
+    var infoArray = [$(".info-bio"), $(".info-contact"), $(".info-gnosis"), $(".info-aquaintly"), $(".info-helio"), $(".info-share")];  
 
-// var myRouter = angular.module("MyRouter", ["ngRoute"]);
-
-// myRouter.config(['$routeProvider', function($routeProvider) {
-//   $routeProvider.
-//     when('/#/', {
-//       templateUrl: 'assets/templates/home.html',
-//       controller: 'MyCtrl'
-//     }).
-//     when('/#/about', {
-//       templateUrl: 'assets/templates/about.html',
-//       controller: 'MyCtrl'
-//     }).
-//     wen('/#/projects', {
-//       templateUrl: 'assets/templates/projects.html',
-//       controller: 'MyCtrl'
-//     }).
-//     otherwise({
-//       redirectTo: '/'
-//     });
-// }]);
-
-// myApp.controller("MyCtrl", ["$scope", "$location", function($scope, $location) {
-//   $scope.go = function(path) {
-//     $location.path(path);
-//   };
-//   $scope.test = "Hello World";
-// }]);
-
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 500);
-        return false;
+    function hideArray () {
+      for (var i = 0; i < infoArray.length; i++) {
+        infoArray[i].hide();
       }
     }
+    function showInfo (thisElement,elementInfo) {
+      hideArray();
+      thisElement.closest(".page-wrapper").find(elementInfo).fadeToggle("slow");
+    }
   });
-});
+Status API Training Shop Blog About Help
+© 2015 GitHub, Inc. Terms Privacy Security Contact
